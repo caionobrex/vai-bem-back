5c4c82d605fac30632e7d8bef83ff46ca1aca7d7
caionobrex <marialoucos@hotmail.com.br>
2022-05-15 19:45:20 -0300

    cors enabled and global pipes added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './database/prisma.service';
@@ -6,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app)
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe())
+  app.enableCors()
+  await app.listen(3001);
 }
 bootstrap();
